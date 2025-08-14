e7d5647bba20587d42a2b58d33c6806d5b042bf2
sulimninkaev <sulimninkaev0@gmail.com>
2025-08-14 12:56:40 +0200

    asd

--- quartz.config.ts
+++ quartz.config.ts
@@ -40,7 +40,7 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#29292bff",
+          light: "#0c0c13ff",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
