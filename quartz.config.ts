86011fe1c013ffe4125646d45a5322d2fa90105b
sulimninkaev <sulimninkaev0@gmail.com>
2025-08-14 12:46:34 +0200

    Change Template

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Sulim's Archive",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -40,7 +40,7 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#161618",
+          light: "#29292bff",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,7 +15,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a href="https://discord.gg/s6uUjQ3Ymz/">Discord{version}</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
